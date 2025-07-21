2883661b81c6bb6440b2f148b46397e8900f3ce9
Mohammad <mohammadcallof138@gmail.com>
2025-07-21 09:49:34 +0330

    add some models and product and some examples and define product list and productDetailView for get

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'products',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import File, Product, Category
+from .serializers import FileSerializer, ProductSerializer, CategorySerializer
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class ProductDetailView(APIView):
+    
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data    
+    
